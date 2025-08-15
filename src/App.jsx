15909a6e60ab01461ad52359d2612b3175529206
AliAbdullahpgr <ilabhaia1234@gmail.com>
2025-08-15 11:51:05 +0500

    login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import Login from './components/Auth/Login'
-import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
-import AdminDashboard from './components/Dashboard/AdminDashboard'
+import React, { useEffect, useState } from "react";
+import Login from "./components/Auth/Login";
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
+import AdminDashboard from "./components/Dashboard/AdminDashboard";
+import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
 const App = () => {
+  const [user, setUser] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = (email, password) => {
+    if (email === "admin@me.com" && password === "admin") {
+      setUser("admin");
+      setLoggedIn(true);
+    } else if (email === "employee@me.com" && password === "employee") {
+      setUser("employee");
+      setLoggedIn(true);
+    } else {
+      alert("Invalid Credentials");
+    }
+  };
   return (
     <>
-      <Login />
-      {/* <EmployeeDashboard /> */}
-      {/* <AdminDashboard /> */}
+      {!loggedIn && <Login handleLogin={handleLogin} />}
+      {loggedIn &&
+        (user === "admin" ? <AdminDashboard /> : <EmployeeDashboard />)}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
